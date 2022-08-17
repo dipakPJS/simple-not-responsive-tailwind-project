0bad260cfde6c6b0d1e291daee1c9cab67ff4c46
Dipak Pokharel <104352102+dipakPJS@users.noreply.github.com>
2022-08-17 16:15:10 +0545

    simple tailwind website
    simple tailwind website  without any responsive layout.

---
+++ advanced tailwind website/resources/js/script.js
@@ -0,0 +1,13 @@
+
+/*javascript auto typed animation start */
+
+var typed = new Typed(".header-box .auto-typed", {
+
+strings: ["I am: Dipak Pokharel", "I am: Front End Developer", "I am: Copy-writer"],
+typeSpeed: 100,
+backSpeed: 100,
+loop: true
+
+});
+
+/* javascript auto typed animation end */
